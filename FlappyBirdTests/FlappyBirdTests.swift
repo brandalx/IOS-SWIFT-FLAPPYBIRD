615125f862611fb53f53c4d03a1fcc946d5816b2
brndalx <113251192+brandalx@users.noreply.github.com>
2024-02-11 21:29:40 -0500

    flappy bird swift

---
+++ FlappyBirdTests/FlappyBirdTests.swift
@@ -0,0 +1,28 @@
+
+import XCTest
+
+class FlappyBirdTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+       
+    }
+    
+    override func tearDown() {
+    
+        super.tearDown()
+    }
+    
+    func testExample() {
+        
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+     
+        self.measure() {
+         
+        }
+    }
+    
+}
